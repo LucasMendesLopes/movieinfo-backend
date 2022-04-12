ec947d7330d7c1b2c93bdb505583a7f29c59165e
lucas lopes <lucasmendeslopes.ti@gmail.com>
2022-04-12 18:35:31 -0300

    primeiro commit

--- src/index.js
+++ src/index.js
@@ -2,15 +2,16 @@ const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
 
-mongoose.connect(
-  "mongodb+srv://lucas_lopes:Bloco740HFF12@cluster0.nlz7p.mongodb.net/movieInfo"
-);
+const { MONGO_URL } = process.env;
+
+mongoose.connect(MONGO_URL);
 
 app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333);
+app.listen(process.env.PORT || 3333);
